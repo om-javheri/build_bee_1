eb4733bd6564658f1641ffdff363840c1eea3eef
om-javheri <javheri.om2003@gmail.com>
2025-12-12 01:06:03 +0530

    upd last

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className='bg-[#171717] w-full flex flex-col min-h-screen '>
-      <BrowserRouter>
+      <BrowserRouter basename="/build_bee_1">
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
