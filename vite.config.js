ad822fcabde991c4ed5f459cae58af759d004c04
om-javheri <133030439+om-javheri@users.noreply.github.com>
2025-12-12 00:57:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://github.com/om-javheri/build_bee_1"
+  base: "/"
 })
